398a2434ac71320a48892fe0d5aa8a3c73eca387
Mark Ellis <ellismarkf@gmail.com>
2020-02-17 23:52:17 -0600

    feat: init commit - mainspring

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
+import Mainspring, { months, weekdays } from './mainspring'
 import './App.css';
+
+const Debug = props => (
+  <code>
+    <pre>
+      {JSON.stringify(props, null, 2)}
+    </pre>
+  </code>
+)
+
+function getFullMonth(daysInMonth) {
+  let month = []
+  for (let i = 0; i <= daysInMonth; i++) {
+
+  }
+}
+
+function Calendar(props) {
+  let {
+    inputString,
+    year,
+    day,
+    dayOfWeek,
+    firstDayOfMonth,
+    month,
+    daysInMonth,
+    today
+  } = props
+  const days = new Array(daysInMonth).fill(0)
+  const weekRowCount = Math.ceil(daysInMonth / 7)
+  return (
+    <React.Fragment>
+      <input type="date" value={inputString} />
+      <div>
+        {days.map((cell, index) => (
+          <span className={index + 1 === day ? 'today' : ''}>{index + 1}</span>
+        ))}
+      </div>
+      <Debug firstDayOfMonth={firstDayOfMonth} firstWeekdayOfMonth={weekdays[firstDayOfMonth].label} />
+    </React.Fragment>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Playground</h2>
       </header>
-    </div>
+      <Mainspring>
+        {Calendar}
+      </Mainspring>
+    </React.Fragment>
   );
 }
 
---
+++ src/mainspring/index.jsx
@@ -0,0 +1,125 @@
+import React from 'react'
+
+export const jan =  0
+export const feb =  1
+export const mar =  2 
+export const apr =  3
+export const may =  4
+export const jun =  5
+export const jul =  6
+export const aug =  7
+export const sep =  8
+export const oct =  9
+export const nov =  10
+export const dec =  11
+
+export const mon = 0
+export const tue = 1
+export const wed = 2
+export const thu = 3
+export const fri = 4
+export const sat = 5
+export const sun = 6
+
+export const months = {
+  [jan]: { index: 0, label: 'January'},
+  [feb]: { index: 1, label: 'February'},
+  [mar]: { index: 2, label: 'March'},
+  [apr]: { index: 3, label: 'April'},
+  [may]: { index: 4, label: 'May'},
+  [jun]: { index: 5, label: 'June'},
+  [jul]: { index: 6, label: 'July'},
+  [aug]: { index: 7, label: 'August'},
+  [sep]: { index: 8, label: 'September'},
+  [oct]: { index: 9, label: 'October'},
+  [nov]: { index: 10, label: 'November'},
+  [dec]: { index: 11, label: 'December'}
+}
+
+export const weekdays = {
+  [mon]: {label: 'Monday', index: 0},
+  [tue]: {label: 'Tuesday', index: 1},
+  [wed]: {label: 'Wednesday', index: 2},
+  [thu]: {label: 'Thursday', index: 3},
+  [fri]: {label: 'Friday', index: 4},
+  [sat]: {label: 'Saturday', index: 5},
+  [sun]: {label: 'Sunday', index: 6},
+}
+
+
+function leftPad(n) {
+  return n < 10 ? `0${n}` : n.toString()
+}
+
+function checkLeapYear(year) {
+  // year is multiple of 4 (except for years evenly divisible by 100, which are not leap years unless evenly divisible by 400)
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      return year % 400 === 0
+    } else {
+      return true
+    }
+  } else {
+    return false
+  }
+}
+
+function getDaysInMonth(month, year) {
+  switch (month) {
+    case feb:
+      return checkLeapYear(year) ? 29 : 28
+    case jan:
+    case mar:
+    case may:
+    case jul:
+    case aug:
+    case oct:
+    case dec:
+      return 31
+    default:
+      return 30
+  }
+}
+
+window.checkLeapYear = checkLeapYear
+
+function toInputString(date) {
+  let year = date.getFullYear()
+  let month = date.getMonth() + 1
+  let day = date.getDate()
+  return `${year}-${leftPad(month)}-${leftPad(day)}`
+}
+
+/**
+ * 
+ * @param {Date} date 
+ */
+function getDateComponents(date) {
+  let year = date.getFullYear()
+  let month = date.getMonth()
+  let day = date.getDate()
+  let dayOfWweek = date.getDay()
+  let todayCopy = new Date()
+  let firstDayOfMonth = new Date(todayCopy.setDate(0)).getDay()
+  return {
+    inputString: toInputString(date),
+    year,
+    day,
+    firstDayOfMonth,
+    dayOfWweek,
+    month,
+    daysInMonth: getDaysInMonth(month, year),
+    today: date
+  }
+}
+
+function Mainspring(props) {
+  let today = new Date()
+  return (
+    <React.Fragment>
+      {props.children(getDateComponents(today))}
+    </React.Fragment>
+  )
+}
+
+export default Mainspring
